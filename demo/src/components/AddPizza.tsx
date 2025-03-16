e8e16e115ea3f5767fe8d9f662a5801f24cbebe4
nayangodhwani-atri <ngodhwan@atrihub.io>
2025-03-15 19:18:22 -0700

    [feat: Design a page to add pizza]
    [ICTRAINING-1895]

--- demo/src/components/AddPizza.tsx
+++ demo/src/components/AddPizza.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import './AddPizza.css';
 
 interface Pizza {
   id: number;
@@ -11,11 +12,11 @@ interface Pizza {
 }
 
 interface PizzaFormProps {
-  setPizza: React.Dispatch<React.SetStateAction<Pizza[]>>; 
+  setPizza: React.Dispatch<React.SetStateAction<Pizza[]>>;
   pizza: Pizza[];
 }
 
-const PizzaForm: React.FC<PizzaFormProps> = ({ setPizza,pizza }) => {
+const PizzaForm: React.FC<PizzaFormProps> = ({ setPizza, pizza }) => {
   const { control, handleSubmit, formState: { errors }, reset } = useForm<Pizza>({
     defaultValues: {
       name: '',
@@ -24,20 +25,17 @@ const PizzaForm: React.FC<PizzaFormProps> = ({ setPizza,pizza }) => {
       delivery: false,
     },
   });
-  
-
   const navigate = useNavigate();
+
   const onSubmit = (data: Pizza) => {
-    const newId = pizza.length > 0 ? Math.max(...pizza.map(p => p.id)) + 1 : 1;
+    const newId = pizza.length > 0 ? Math.max(...pizza.map((p) => p.id)) + 1 : 1;
     const newPizza: Pizza = {
-      id: newId, 
+      id: newId,
       name: data.name,
       toppings: data.toppings,
       Favourite: data.Favourite,
       delivery: data.delivery,
     };
-
-    console.log(newPizza.name);
     setPizza((prevPizza) => [...prevPizza, newPizza]);
     reset();
     navigate('/home');
@@ -46,84 +44,93 @@ const PizzaForm: React.FC<PizzaFormProps> = ({ setPizza,pizza }) => {
   const toppingOptions = ['Cheese', 'Pepperoni', 'Black Olives', 'Beef', 'BBQ Sauce', 'Mushrooms'];
 
   return (
-    <div>
-      <h3>Add a New Pizza</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        
-        <label>
-          Pizza Name:
-          <Controller
-            name="name"
-            control={control}
-            defaultValue=""
-            rules={{ required: 'Pizza name is required', maxLength: { value: 100, message: 'Name cannot be more than 100 characters' } }}
-            render={({ field }) => <input {...field} />}
-          />
-        </label>
-        {errors.name && <p>{errors.name.message}</p>}
-        <br />
+    <div className="add-pizza-page">
+      <div className="add-pizza-content">
+        <div className="form-container">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-row">
+              <span className="form-label">Pizza Name:</span>
+              <div className="form-input">
+                <Controller
+                  name="name"
+                  control={control}
+                  rules={{ required: 'Pizza name is required', maxLength: { value: 100, message: 'Name cannot be more than 100 characters' } }}
+                  render={({ field }) => <input type="text" {...field} />}
+                />
+                {errors.name && <p>{errors.name.message}</p>}
+              </div>
+            </div>
 
-        
-        <label>Toppings:</label>
-        <div>
-          {toppingOptions.map((topping) => (
-            <label key={topping} style={{ marginRight: '15px' }}>
-              <Controller
-                name="toppings"
-                control={control}
-                rules={{ required: 'At least one topping is required' }}
-                render={({ field }) => (
-                  <input
-                    type="checkbox"
-                    value={topping}
-                    checked={field.value.includes(topping)}
-                    onChange={(e) => {
-                      const newValue = e.target.checked
-                        ? [...field.value, topping]
-                        : field.value.filter((t: string) => t !== topping);
-                      field.onChange(newValue);
-                    }}
-                  />
-                )}
-              />
-              {topping}
-            </label>
-          ))}
+            <div className="form-row">
+              <span className="form-label">Toppings:</span>
+              <div className="form-input">
+                <div className="checkbox-group">
+                  {toppingOptions.map((topping) => (
+                    <label key={topping}>
+                      <Controller
+                        name="toppings"
+                        control={control}
+                        rules={{ required: 'At least one topping is required' }}
+                        render={({ field }) => (
+                          <input
+                            type="checkbox"
+                            value={topping}
+                            checked={field.value.includes(topping)}
+                            onChange={(e) => {
+                              const newValue = e.target.checked
+                                ? [...field.value, topping]
+                                : field.value.filter((t: string) => t !== topping);
+                              field.onChange(newValue);
+                            }}
+                          />
+                        )}
+                      />
+                      {topping}
+                    </label>
+                  ))}
+                </div>
+                {errors.toppings && <p>{errors.toppings.message}</p>}
+              </div>
+            </div>
+
+            <div className="form-row">
+              <span className="form-label">Favourite:</span>
+              <div className="form-input">
+                <Controller
+                  name="Favourite"
+                  control={control}
+                  render={({ field }) => (
+                    <input
+                      type="checkbox"
+                      checked={field.value}
+                      onChange={(e) => field.onChange(e.target.checked)}
+                    />
+                  )}
+                />
+              </div>
+            </div>
+
+            <div className="form-row">
+              <span className="form-label">Delivery:</span>
+              <div className="form-input">
+                <Controller
+                  name="delivery"
+                  control={control}
+                  render={({ field }) => (
+                    <input
+                      type="checkbox"
+                      checked={field.value}
+                      onChange={(e) => field.onChange(e.target.checked)}
+                    />
+                  )}
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="button">Add Pizza</button>
+          </form>
         </div>
-        {errors.toppings && <p>{errors.toppings.message}</p>}
-        <br />
-
-        
-        <label>
-  Favourite:
-  <Controller
-    name="Favourite"
-    control={control}
-    defaultValue={false} // Change default value to boolean false instead of "No"
-    render={({ field }) => (
-      <input
-        type="checkbox"
-        checked={field.value} // Use boolean value
-        onChange={(e) => field.onChange(e.target.checked)} // Update value as boolean
-      />
-    )}
-  />
-</label>
-
-    
-        <label>
-          Delivery:
-          <Controller
-            name="delivery"
-            control={control}
-            defaultValue={false}
-            render={({ field }) => <input type="checkbox" checked={field.value} onChange={(e) => field.onChange(e.target.checked)}/>}
-          />
-        </label>
-        <br />
-
-        <button type="submit">Add Pizza</button>
-      </form>
+      </div>
     </div>
   );
 };
